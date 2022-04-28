abb056d7fa515fa1d5750f1817acd1ef8deb5f9c
HanishaDurgia <67540930+HanishaDurgia@users.noreply.github.com>
2022-04-29 00:52:03 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+
+
+class add 
+	{
+	public static void main(String[]args)
+		{
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+
+			System.out.println("Addition of two numbers " +a+ " " +b+ "is:" +(a+b));
+			
+				}
+
+
+	}
